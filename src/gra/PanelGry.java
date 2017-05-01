6016cd522a4988ed64681b7f5cf308999931df00
Marcin Kacik <marcinkacik@interia.pl>
2017-05-02 01:07:26 +0200

    Dodanie skalowania obrazu

--- src/gra/PanelGry.java
+++ src/gra/PanelGry.java
@@ -32,14 +32,17 @@ public class PanelGry extends JPanel {
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         Font czcionka = new Font("Sans Serif", Font.BOLD, 20);
 
+        int start_rysowania = (int) ((d.getWidth() - obraz.getWidth()) / 2);
+
+
         for (int b = 0; b <= 4; b++)
             for (int a = 0; a <= 4; a++) {
 
                 //g2d.drawImage(tab_obrazkow[b][a], a * (rozmiar_kawalka + 2), b * (rozmiar_kawalka + 2), this); //rysowanie obrazków
                 //g.setFont(czcionka);
                 //g.setColor(Color.green);
-               // g.drawString(String.valueOf(tab_numerow[a][b]), ((a + 1) * (rozmiar_kawalka)) - 20, (b + 1) * (rozmiar_kawalka));
-                g2d.drawImage(obraz, (int)(( d.getWidth() - obraz.getWidth() )/2) , 0, this);
+                //g.drawString(String.valueOf(tab_numerow[a][b]), ((a + 1) * (rozmiar_kawalka)) - 20, (b + 1) * (rozmiar_kawalka));
+                g2d.drawImage(obraz, start_rysowania, 0, this);
             }
     }
 }
